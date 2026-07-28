ea5a06acdb0325d47d40a3e868b5ed4c36136531
bobbydimas <kulz4real@gmail.com>
2026-07-28 17:50:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
-const CACHE = 'agency-invoicer-v1';
+const CACHE = 'agency-invoicer-v2';
 const APP_FILES = ['/', 'index.html', 'styles/main.css', 'styles/print.css', 'js/config.js', 'js/app.js', 'assets/invoice-wave.png', 'assets/icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+          caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
